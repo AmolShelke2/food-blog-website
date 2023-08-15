2b3ef4f7aa9db8ed19229b46fe01d305f73e09d6
Amol <shelkeamol897@gmail.com>
2023-08-15 14:34:53 +0530

    [feat]: banner component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import About from "@/components/About";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen max-w-[1440px] border-2 border-black justify-center mx-auto">
-      hello world
+    <main className="flex flex-col h-full max-w-[1440px] justify-center mx-auto">
+      <Banner />
+      <About />
     </main>
   );
 }
